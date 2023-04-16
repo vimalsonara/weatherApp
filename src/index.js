ae22b82bd193a96972d2b7ba61081dc3e4d5352b
Vimal Sonara <vvsonara95@gmail.com>
2023-04-16 15:59:14 +0530

    Update UI on form submit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
+import {getCity, getWeather} from './module/forecast';
 import './style.css';
+
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUi = (data) => {
+    const {cityDetails, weather} = data;
+
+    console.log(cityDetails, weather);
+
+    details.innerHTML = `
+    <h5>${cityDetails.EnglishName}</h5>
+    <div class="weather-condition">
+        ${weather.WeatherText}
+    </div>
+    <div class="temp">
+        <span>TEMP</span>
+        <span>${weather.Temperature.Metric.Value} &deg;C</span>
+    `;
+
+    if(card.classList.contains('card-modal')) {
+        card.classList.remove('card-modal');
+    }
+}
+
+const updateCity = async (city) => {
+    
+    const cityDetails = await getCity(city);
+    const weather = await getWeather(cityDetails.Key)
+
+    return {cityDetails, weather};
+}
+
+cityForm.addEventListener('submit', e => {
+    
+    e.preventDefault();
+
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+    updateCity(city)
+        .then(data => updateUi(data))
+        .catch(error => alert(error))
+})
